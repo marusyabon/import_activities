6f04a1ba0bfe2d8d5cc974728007361e9176d4a9
Maryna Bandarenka <maryna.bandarenka@nba-070-v1b-lt.local>
2024-05-08 14:42:40 +0300

    feat: add try/catch in the main process

--- src/importDataToDB/index.js
+++ src/importDataToDB/index.js
@@ -1,4 +1,4 @@
 export {default as importNotes} from './importNotes.js';
 export {default as importCalls} from './importCalls.js';
 export {default as importTasks} from './importTasks.js';
-export {default as importEmails} from './ImportEmails.js';
+export {default as importEmails} from './importEmails.js';
--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import { latestCallID, latestEmailID, latestNoteID, latestTaskID } from './getLatestIDs.js';
 import { importNotes, importCalls, importTasks, importEmails } from './importDataToDB/index.js';
 
+async function importHsActivitiesToDB() {
+  await Promise.all([
+    importNotes(latestNoteID),
+    importCalls(latestCallID),
+    importTasks(latestTaskID),
+    importEmails(latestEmailID)
+  ]);
+}
 
-importNotes(latestNoteID);
-importCalls(latestCallID);
-importTasks(latestTaskID);
-importEmails(latestEmailID);
+try {
+  await importHsActivitiesToDB();
+} catch(e) {
+  console.error('Main process failed');
+  console.error(e);
+}
